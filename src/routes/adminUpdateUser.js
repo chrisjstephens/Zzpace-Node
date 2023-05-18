9bd6c2eac9ddc221bf6dcc3f9c883e4402f50d51
Chris Stephens <chrisjstephens@yahoo.com>
2023-05-18 09:12:32 -0400

    May 18 - Node updates

--- src/routes/adminUpdateUser.js
+++ src/routes/adminUpdateUser.js
@@ -6,7 +6,7 @@ var users = require('../models/user.js');
 router.put('/api/admin/updateUser/:username', (req, res, next) => {
   //Update id
   //Todo: anyone with a valid token can update, make it so that only customer1 can update customer1, admin update admin, etc
-  users.findOneAndUpdate({ 'username': req.username }, { $set: req.body }, { runValidators: true, context: 'query' }, function (err) {
+  users.findOneAndUpdate({ 'username': req.params.username }, { $set: req.body }, { runValidators: true, context: 'query' }, function (err) {
     if (!err)  {
         res.json('User has been updated');
     } else {
